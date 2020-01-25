7f08a61d6e179ee56e0f492e9611a50865e9e19b
Bratislav <37560533+BratislavStojanovic@users.noreply.github.com>
2020-01-25 17:37:47 +0100

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,79 @@
+let mainRow = document.querySelector('#main-row');
+let addBtn = document.querySelector('#addBtn');
+let input = document.querySelector('input');
+
+addBtn.addEventListener('click', function() {
+  let inputVal = input.value;
+  let xml = new XMLHttpRequest();
+  xml.open('POST', '/save');
+  xml.onreadystatechange = function() {
+    if (xml.readyState == 4 && xml.status == 200) {
+      displayTodos();
+    }
+  };
+  xml.setRequestHeader('Content-Type', 'application/json');
+  xml.send(JSON.stringify({ msg: inputVal }));
+});
+
+function displayTodos() {
+  let data = new Promise((resolve, reject) => {
+    let xml = new XMLHttpRequest();
+    xml.open('GET', '/get_data');
+    xml.onreadystatechange = () => {
+      if (xml.readyState == 4 && xml.status == 200) {
+        resolve(JSON.parse(xml.responseText));
+      }
+    };
+
+    xml.send();
+  });
+  data.then(data => {
+    let text = '';
+    for (let i = 0; i < data.length; i++) {
+      text += `
+      <div class="col-4">
+              <div class="card">
+                <div class="card-header">
+                  <button class="btn btn-sm btn-secondary float-left">
+                    Todo: ${i + 1}
+                  </button>
+                  <button class="btn btn-sm btn-success float-right">
+                    ${data[i].date}
+                  </button>
+                </div>
+                <div class="card-body text-center">
+                  <h3>${data[i].msg}</h3>
+                </div>
+                <div class="card-footer text-center">
+                  <button data-id=${
+                    data[i]._id
+                  } class="btn btn-sm btn-danger">Delete</button>
+                </div>
+              </div>
+            </div>
+      
+      `;
+      mainRow.innerHTML = text;
+      let allDeleteBtns = document.querySelectorAll('[data-id]');
+      for (let i = 0; i < allDeleteBtns.length; i++) {
+        allDeleteBtns[i].addEventListener('click', deleteTodo);
+      }
+    }
+  });
+}
+
+displayTodos();
+
+function deleteTodo() {
+  console.log(this.getAttribute(['data-id']));
+
+  let xml = new XMLHttpRequest();
+  xml.open('POST', '/delete');
+  xml.onreadystatechange = () => {
+    if (xml.readyState == 4 && xml.status == 200) {
+      displayTodos();
+    }
+  };
+  xml.setRequestHeader('Content-Type', 'application/json');
+  xml.send(JSON.stringify({ id: this.getAttribute('data-id') }));
+}
